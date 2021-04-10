5c32e7466a9d68b2682b8086db23113af151a583
Vanek <vanevo00@gmail.com>
2021-04-10 11:18:57 +0200

    feat: user ERC721 openzeppelin token, basic tests

--- contracts/Gems.sol
+++ contracts/Gems.sol
@@ -1,9 +1,11 @@
 pragma solidity >=0.8.3 <0.9.0;
 
-contract Gems {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Gems is ERC721 {
     address public minter;
 
-    constructor() {
+    constructor() ERC721("Gems", "GEM") {
         minter = msg.sender;
     }
 }
--- test/gems.test.js
+++ test/gems.test.js
@@ -1,8 +1,28 @@
 const Gems = artifacts.require('Gems');
 
 contract('Gems contract', async ([owner]) => {
+    let gemsContract
+
+    before(async () => {
+        gemsContract = await Gems.deployed()
+    })
+
+    it('should have an address', async () => {
+        const address = gemsContract.address
+        assert.notEqual(address, 0x0)
+        assert.notEqual(address, '')
+        assert.notEqual(address, null)
+        assert.notEqual(address, undefined)
+    })
+
+    it('should have a name and a symbol', async () => {
+        const name = await gemsContract.name()
+        const symbol = await gemsContract.symbol()
+        assert.equal(name, 'Gems')
+        assert.equal(symbol, 'GEM')
+    })
+
     it('should set contract owner as minter', async () => {
-        const gemsContract = await Gems.deployed()
         const minterAddress = await gemsContract.minter()
         assert.equal(owner, minterAddress)
     })
