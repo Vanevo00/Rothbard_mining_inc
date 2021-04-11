6d6a33f87d7d13d6254fa1d92592837fe9169162
Vanek <vanevo00@gmail.com>
2021-04-11 21:41:24 +0200

    feat: basic mint function and tests

--- contracts/Gems.sol
+++ contracts/Gems.sol
@@ -1,11 +1,33 @@
 pragma solidity >=0.8.3 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Gems is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter public tokenIds;
     address public minter;
+
+    struct Attributes {
+        string weight;
+        string cut;
+        string gemType;
+        string clarity;
+    }
+    mapping(uint => Attributes) public gemAttributes;
 
     constructor() ERC721("Gems", "GEM") {
         minter = msg.sender;
+    }
+
+    function mint(string memory weight, string memory cut, string memory gemType, string memory clarity) public returns (uint256) {
+        require(msg.sender == minter, "Only the contract owner can mint new gems");
+        tokenIds.increment();
+
+        uint256 newItemId = tokenIds.current();
+        _safeMint(minter, newItemId);
+        gemAttributes[newItemId] = Attributes(weight, cut, gemType, clarity);
+
+        return newItemId;
     }
 }
--- test/gems.test.js
+++ test/gems.test.js
@@ -7,23 +7,54 @@ contract('Gems contract', async ([owner]) => {
         gemsContract = await Gems.deployed()
     })
 
-    it('should have an address', async () => {
-        const address = gemsContract.address
-        assert.notEqual(address, 0x0)
-        assert.notEqual(address, '')
-        assert.notEqual(address, null)
-        assert.notEqual(address, undefined)
+    describe('deployment', async () => {
+        it('should have an address', async () => {
+            const address = gemsContract.address
+            assert.notEqual(address, 0x0)
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+        })
+
+        it('should have a name and a symbol', async () => {
+            const name = await gemsContract.name()
+            const symbol = await gemsContract.symbol()
+            assert.equal(name, 'Gems')
+            assert.equal(symbol, 'GEM')
+        })
+
+        it('should set contract owner as minter', async () => {
+            const minterAddress = await gemsContract.minter()
+            assert.equal(owner, minterAddress)
+        })
     })
 
-    it('should have a name and a symbol', async () => {
-        const name = await gemsContract.name()
-        const symbol = await gemsContract.symbol()
-        assert.equal(name, 'Gems')
-        assert.equal(symbol, 'GEM')
-    })
+    describe('minting', async () => {
+        before(async () => {
+            await gemsContract.mint('0.37', 'brilliant', 'diamond', 'VS1')
+        })
 
-    it('should set contract owner as minter', async () => {
-        const minterAddress = await gemsContract.minter()
-        assert.equal(owner, minterAddress)
+        it('should increment tokenIds', async () => {
+            const tokenIds = await gemsContract.tokenIds()
+            assert.equal(tokenIds, '1')
+        })
+
+        it('should increment balanceOf contract owner', async () => {
+            const ownerBalance = await gemsContract.balanceOf(owner)
+            assert.equal(ownerBalance, '1')
+        })
+
+        it('should assign contract owner as the owner of the minted gem', async () => {
+            const mintedGemOwnerAddress = await gemsContract.ownerOf('1')
+            assert.equal(mintedGemOwnerAddress, owner)
+        })
+
+        it('should save attributes of the gem', async () => {
+            const mintedGemAttributes = await gemsContract.gemAttributes('1')
+            assert.equal(mintedGemAttributes.weight, '0.37')
+            assert.equal(mintedGemAttributes.cut, 'brilliant')
+            assert.equal(mintedGemAttributes.gemType, 'diamond')
+            assert.equal(mintedGemAttributes.clarity, 'VS1')
+        })
     })
 })
