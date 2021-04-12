4f0ff1b0f1577a8fa8e735c2c1601d95009e0e4c
Vanek <vanevo00@gmail.com>
2021-04-12 21:10:09 +0200

    feat: initialize react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,44 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Navbar, Nav } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Buy from './Buy'
+import Sell from './Sell'
+import Mint from './Mint'
 
 const App = () => {
     return (
-        <h1>
-           Rothbard mining Inc.
-        </h1>
+        <Router>
+            <Navbar bg="primary" variant="dark">
+                <LinkContainer to="/">
+                    <Navbar.Brand href="#home">Rothbard Mining Inc.</Navbar.Brand>
+                </LinkContainer>
+                <Nav className="mr-auto">
+                    <LinkContainer to="/buy">
+                        <Nav.Link>Buy</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to="/sell">
+                        <Nav.Link>Sell</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to='/mint'>
+                        <Nav.Link>Mint</Nav.Link>
+                    </LinkContainer>
+                </Nav>
+            </Navbar>
+
+            <Switch>
+                <Route path={['/', '/buy']} exact>
+                    <Buy/>
+                </Route>
+                <Route path="/sell" exact>
+                    <Sell/>
+                </Route>
+                <Route path="/mint" exact>
+                    <Mint/>
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 
---
+++ client/src/Buy.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Buy = () => {
+    return (
+        <h1>
+           Buy
+        </h1>
+    )
+}
+
+export default Buy
