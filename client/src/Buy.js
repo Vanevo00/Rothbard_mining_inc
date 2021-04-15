ee7222408b0195066508833207cf8d9d78fefcc4
Vanek <vanevo00@gmail.com>
2021-04-15 08:51:31 +0200

    feat: link to metamask if no wallet found

--- client/src/Buy.js
+++ client/src/Buy.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import { Spinner, Button } from 'react-bootstrap'
 
 const Buy = ({ isLoading, userId }) => {
+  if (isLoading) {
     return (
-      <div className='bg-danger flex-grow-1'>
-        <h1>
-          Buy
-        </h1>
+      <div className='flex-grow-1 d-flex justify-content-center align-items-center'>
+        <Spinner animation='border' />
       </div>
     )
+  }
+
+  if (!userId) {
+    return (
+      <div className='flex-wrap d-flex justify-content-center mt-5'>
+        <h4 className='w-100 text-center'>No ethereum wallet detected. In order to run this application, MetaMask or other ethereum wallet software is necessary.</h4>
+        <a href='https://metamask.io/download.html' target='_blank'><Button variant='primary' className='mt-5'>Download MetaMask</Button></a>
+      </div>
+    )
+  }
+
+  return (
+    <h1>Buy</h1>
+  )
 }
 
 export default Buy
